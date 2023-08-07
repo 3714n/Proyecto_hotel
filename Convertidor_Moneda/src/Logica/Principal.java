05501738ef4aeeb8db0eb861dd9775e7f5618d62
Etian <ethiamgt@gmail.com>
2023-08-07 11:33:04 -0500

    nuevo

---
+++ Convertidor_Moneda/src/Logica/Principal.java
@@ -0,0 +1,12 @@
+package Logica;
+import Parte_Grafica.Ventana_Principal;
+
+public class Principal extends Ventana_Principal{
+
+	public static void main(String[] args) {
+		Ventana_Principal activador = new Ventana_Principal();
+			activador.setVisible(true);
+			activador.setLocationRelativeTo(null);
+	}
+
+}
